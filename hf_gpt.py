d24d04abc2e16077996588bfc07f05d06f6ed16e
TimS <jshen44@fordham.edu>
2024-07-12 16:03:45 +0800

    v4 notebook and v5 draft

--- hf_gpt.py
+++ hf_gpt.py
@@ -182,6 +182,48 @@ class GPT(nn.Module):
                     )
             return logits, loss
 
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+    
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+    
+        # preview the decay and nodecay layers
+        tmp_decay_params = []
+        tmp_nodecay_params = []
+        for n, p in param_dict.items():
+            if p.dim() >= 2 and len(tmp_decay_params) < 3:
+                tmp_decay_params.append(n)
+            elif p.dim() < 2 and len(tmp_nodecay_params) < 3:
+                tmp_nodecay_params.append(n)
+            if len(tmp_decay_params) >=3 and len(tmp_nodecay_params) >= 3:
+                break
+        cprint(tmp_decay_params, tmp_nodecay_params)
+        
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+    
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        cprint(f"{len(decay_params)}, {num_decay_params:,}")
+        cprint(f"{len(nodecay_params)}, {num_nodecay_params:,}")
+    
+        # Create AdamW optimizer and use the fused version if it is available
+        # NOTE: fused is only available on CUDA devices, but is much faster
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        cprint(f"using fused AdamW: {use_fused}")
+    
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
     @classmethod
     def from_pretrained(cls, model_type):
         '''https://youtu.be/l8pRSuU81PU?t=1830
